64b5c66234b79d513de49e4e157bbb576b421ac1
Matan157 <matan.uchen@gmail.com>
2015-12-17 22:39:47 +0000

    first draft

--- feeder.py
+++ feeder.py
@@ -12,32 +12,38 @@ import time
 
 servoPin = 21 # Pin powering sending signal to servo.
 feedTime = 5 # How long it should rotate. (Seconds)
-startTime = 0 # When the program starts.
+startTime = time.time() # When the program starts.
 
-b = Button(4)
+b = Button(4) # Button on pin 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPin, GPIO.OUT)
 servo = GPIO.PWM(servoPin, 50)
 
+# Methon for when button is pressed. Feeds for indefinite amount of time.
+def feedButton():
+    servo.start(2.5)
+    time.sleep(5)
+# Stops the servo.
+def stop():
+    servo.stop()
+
+# Feed method.
 def feed():
     servo.start(2.5)
     time.sleep(feedTime)
-    servo.stop()
-feed()
-b.when_pressed = feed
-b.when_released = servo.stop()
+    stop()
+
+b.when_released = stop
 try:
     while True:
-        if startTime == 0:
+        elapsedTime = time.time() - startTime
+        if elapsedTime >= 86400 or b.is_pressed:
             feed()
             startTime = time.time()
-        elapsedTime = time.time() - startTime
-        if elapsedTime >= 86400:
-            feed()
     time.sleep(.1)
 
-except:
+except KeyboardInterrupt:
     servo.stop()
     GPIO.cleanup()
 
